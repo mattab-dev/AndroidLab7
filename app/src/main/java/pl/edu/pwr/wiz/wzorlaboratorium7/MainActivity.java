336f4fc3305be10b4b794d5d1f105f0a53b1fe6a
Remigiusz Samborski <rsamborski@gmail.com>
2018-05-04 15:35:32 +0200

    Aktualizacja gradle i dodanie ignore'ow.

--- app/src/main/java/pl/edu/pwr/wiz/wzorlaboratorium7/MainActivity.java
+++ app/src/main/java/pl/edu/pwr/wiz/wzorlaboratorium7/MainActivity.java
@@ -3,8 +3,8 @@ package pl.edu.pwr.wiz.wzorlaboratorium7;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
